50a823402e40ee2cffc356fd5deac0054afdd77e
Agata Jewko <agataj@fathomlondon.com>
2018-04-17 11:17:04 +0100

    Add typography

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography";
+import judahTheme from "typography-theme-judah";
+
+const typography = new Typography(judahTheme);
+// const typography = new Typography({ baseFontSize: "18px" });
+
+export default typography;
