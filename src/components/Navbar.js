ff7c954d2b29a998654aadcb087adbe8b2eb5356
Agata Jewko <agataj@fathomlondon.com>
2018-04-17 14:51:53 +0100

    Edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,55 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React, { PureComponent } from "react";
+import Link from "gatsby-link";
 
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import github from "../img/github-icon.svg";
+import logo from "../img/logo.svg";
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-    </div>
-  </nav>
-)
+class Navbar extends PureComponent {
+  state = {
+    isActive: false
+  };
+  getStatus = () => {
+    const STATES = { true: "is-active", false: "" };
+    return STATES[this.state.isActive];
+  };
+  render() {
+    return (
+      <nav className="navbar is-transparent">
+        <div className="container">
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              <figure className="image">
+                <h1 className="has-text-weight-bold is-size-1">W</h1>
+              </figure>
+            </Link>
+            <div
+              class={`navbar-burger burger ${this.getStatus()}`}
+              data-target="navMenuDocumentation"
+              onClick={() =>
+                this.setState(prevState => ({ isActive: !prevState.isActive }))
+              }
+            >
+              <span />
+              <span />
+              <span />
+            </div>
+          </div>
 
-export default Navbar
+          <div class={`navbar-menu ${this.getStatus()}`}>
+            <div className="navbar-start" />
+            <div className="navbar-end">
+              <Link className="navbar-item" to="/about">
+                About
+              </Link>
+              <Link className="navbar-item" to="/products">
+                Products
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
