887da54c04010a4540d5e081f77137b437c6766c
Agata Jewko <agataj@fathomlondon.com>
2018-05-11 15:12:51 +0100

    New typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography";
-import judahTheme from "typography-theme-judah";
+import moragaTheme from "typography-theme-moraga";
 
-const typography = new Typography(judahTheme);
+const typography = new Typography(moragaTheme);
 // const typography = new Typography({ baseFontSize: "18px" });
 
 export default typography;
